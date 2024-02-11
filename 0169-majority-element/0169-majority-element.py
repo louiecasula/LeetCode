e2df6353bf764d6de378836e6b31b0bef1da2f55
Louie Casula <121182711+louiecasula@users.noreply.github.com>
2024-02-11 18:04:43 -0500

    Time: 136 ms (61.11%), Space: 18.1 MB (63.51%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,14 +1,6 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        freq = dict()
-        # Map elements,
-        for num in nums:
-            print(num)
-            if num in freq:
-                freq[num] += 1
-            if num not in freq:
-                freq[num] = 1
-            # If the value of the current key > n / 2
-            if freq[num] > len(nums) / 2:
-                # Return element
-                return num
+        # Sort array
+        nums.sort()
+        # Return element at the midpoint
+        return nums[int(len(nums)/2)]
