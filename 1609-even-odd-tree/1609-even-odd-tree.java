ed4434b6d9722c65283cc2bfab3c54cc1fd90139
Louie Casula <121182711+louiecasula@users.noreply.github.com>
2024-02-29 10:28:41 -0500

    Time: 960 ms (5.08%), Space: 107.3 MB (5.08%) - LeetHub

---
+++ 1609-even-odd-tree/1609-even-odd-tree.java
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    HashMap<Integer, Stack<Integer>> map = new HashMap();
+    boolean even = true;
+
+    public boolean isEvenOddTree(TreeNode root) {
+        dfs(root, 0);
+        for (Integer key: map.keySet()) {
+            System.out.print(key + "(");
+            if (key % 2 == 0) { even = true; }
+            else { even = false; }
+            int current = map.get(key).pop();
+            if ((even && current % 2 == 0) || (!even && current % 2 == 1)) { return false; }
+            System.out.print(even + "): ");
+            System.out.print(current + " ");
+            while (map.get(key).size() > 0) {
+                if ((even && current % 2 == 0) || (!even && current % 2 == 1)) { return false; }
+                int next = map.get(key).pop();
+                System.out.print(next + " ");
+                if ((even && next >= current) || (!even && next <= current)) { return false; }
+                current = next;
+            }
+            System.out.println();
+        }
+        return true;
+    }
+
+    public void dfs(TreeNode root, int depth) {
+        if (root == null) { return; }
+
+        if (map.get(depth) == null) {
+            map.put(depth, new Stack<Integer>());
+        } map.get(depth).push(root.val); 
+        
+
+        dfs(root.left, depth + 1);
+        dfs(root.right, depth + 1);
+    }
+}
