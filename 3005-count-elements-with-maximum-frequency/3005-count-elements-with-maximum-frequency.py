e66f70b206201271c00f9c7380567b08e2b7a07d
Louie Casula <121182711+louiecasula@users.noreply.github.com>
2024-03-08 09:38:52 -0500

    Time: 48 ms (25.91%), Space: 16.5 MB (97.35%) - LeetHub

--- 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.py
+++ 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.py
@@ -3,16 +3,17 @@ class Solution:
         # Map frequency of each num in nums keeping track of the highest frequency
         map = {}
         max = 1
+        out = 0
         for num in nums:
             if num not in map:
                 map[num] = 1
             else:
                 map[num] += 1
-                if map[num] > max:
-                    max = map[num]
-        # Iterate map, if freq == max, add max to output
-        out = 0
-        for num in map:
+            if map[num] > max:
+                max = map[num]
+                out = max
+                continue
             if map[num] == max:
                 out += max
+        # Iterate map, if freq == max, add max to output
         return out
