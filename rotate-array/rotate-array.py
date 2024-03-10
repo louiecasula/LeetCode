1b986c2903d490c7211260d75c6681904bb06dc8
theleuth <121182711+louiecasula@users.noreply.github.com>
2024-03-10 09:31:27 -0400

    Time: 1126 ms, Memory: 28.1 MB - LeetHub

--- rotate-array/rotate-array.py
+++ rotate-array/rotate-array.py
@@ -4,8 +4,7 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         # If k is greater than or equal to length, subtract length from it until it is less than length
-        while k >= len(nums):
-            k -= len(nums)
+        k %= len(nums)
         # Iterate for k, pop each val and insert at beginning of list
         for i in range(k):
             x = nums.pop()
