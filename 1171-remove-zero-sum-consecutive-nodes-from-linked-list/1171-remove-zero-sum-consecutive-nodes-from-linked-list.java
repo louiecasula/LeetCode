544429f968293928d911da41001dc5fa8efe7d3b
theleuth <121182711+louiecasula@users.noreply.github.com>
2024-03-12 11:01:26 -0400

    Time: 104 ms (5.14%), Space: 44.8 MB (5.93%) - LeetHub

--- 1171-remove-zero-sum-consecutive-nodes-from-linked-list/1171-remove-zero-sum-consecutive-nodes-from-linked-list.java
+++ 1171-remove-zero-sum-consecutive-nodes-from-linked-list/1171-remove-zero-sum-consecutive-nodes-from-linked-list.java
@@ -10,42 +10,29 @@
  */
 class Solution {
     public ListNode removeZeroSumSublists(ListNode head) {
-        //// Convert to array
-        ArrayList<Integer> array = new ArrayList();
-        ListNode curr = head;
-        while (curr != null) {
-            array.add(curr.val);
-            curr = curr.next;
+        //// Hash table, prefix sum method ////
+        // Make a dummy node and keep the prefix sum
+        ListNode dummy = new ListNode(0, head);
+        int prefixSum = 0;
+        // Map the running sum with the head's value
+        Map<Integer, ListNode> map = new HashMap();
+        map.put(prefixSum, dummy);
+        while (head != null) {
+            prefixSum += head.val;
+            map.put(prefixSum, head);
+            head = head.next;
         }
-        //// While there is a subarray that sums to zero, delete it
-        // Nested for loop
-        for (int i = 0; i < array.size() - 1; i++) {
-            int sum = array.get(i);
-            for (int j = i + 1; j < array.size(); j++) {
-                sum += array.get(j);
-                // If sum equals 0, 
-                if (sum == 0) {
-                    for (int k = i; k <= j; k++) {
-                        array.set(k, 0);
-                    }
-                    i = j;
-                    break;
-                }
-            }
+        for (Integer key: map.keySet()) {
+            System.out.printf("key: %d, val: %d\n", key, map.get(key).val);
         }
-        array.removeAll(Arrays.asList(0));
-        if (array.size() == 0) { return null; }
-        //// Convert back to linked list (recycle the original (; )
-        curr = head;
-        for (int i = 0; i < array.size(); i++) {
-            if (array.get(i) != 0) {
-                curr.val = array.get(i);
-                if (i != array.size() - 1) {
-                    curr = curr.next;
-                }
-            }
+        // Go through linked list again, making the next node equal to the map's value of the sum
+        head = dummy;
+        prefixSum = 0;
+        while (head != null) {
+            prefixSum += head.val;
+            head.next = map.get(prefixSum).next;
+            head = head.next;
         }
-        curr.next = null;
-        return head;
+        return dummy.next;
     }
 }
