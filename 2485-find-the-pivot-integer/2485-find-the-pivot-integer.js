3547bb1211d48e49ed0a051c988f9750cf79dd80
theleuth <121182711+louiecasula@users.noreply.github.com>
2024-03-13 09:50:58 -0400

    Time: 108 ms (13.85%), Space: 55.5 MB (6.15%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.js
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.js
@@ -4,12 +4,13 @@
  */
 var pivotInteger = function(n) {
     let arr = [];
+    let pref_sum = 0;
     for (let i = 1; i <= n; i++) { arr.push(i); };
     // Iterate from 1 -> n
     for (let i = 0; i < n; i++) {
         // If sum 1 -> i == sum i -> n, return i
-        if (arr.slice(0, i + 1).reduce((sum, curr) => sum + curr) === 
-        arr.slice(i, n).reduce((sum, curr) => sum + curr)) {
+        pref_sum += i + 1;
+        if (pref_sum === arr.slice(i, n).reduce((sum, curr) => sum + curr)) {
             return i + 1;
         }
     }
