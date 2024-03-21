e33184a21691ba5bf0f1b118a41d7b1d7dc0eaa3
theleuth <121182711+louiecasula@users.noreply.github.com>
2024-03-21 10:12:41 -0400

    Time: 74 ms (50.05%), Space: 52 MB (86.94%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.js
+++ 0029-divide-two-integers/0029-divide-two-integers.js
@@ -4,22 +4,27 @@
  * @return {number}
  */
 var divide = function(dividend, divisor) {
+    // Edgecase
     if (dividend == (2**31 * -1) && divisor == -1) { return 2**31 -1; }
+    // Set a boolean for pos or neg sign quotient and keep a quotient(counter) variable
     let pos = (dividend > 0) == (divisor > 0);
-    if (divisor == 1) { return dividend; }
-    if (divisor == -1) { return dividend * divisor; }
     let quotient = 0;
+    // Make dividend & divisor positive regardless of original sign
     dividend = Math.abs(dividend), divisor = Math.abs(divisor);
+    // While the dividend - divisor is at least 0,
     while (dividend - divisor >= 0) {
         let ndivisor = divisor;
         let factor = 0;
+        // While the dividend - divisor * 2 is at least 0, keep doubling divisor, increment a factor variable
         while (dividend - (ndivisor * 2) >= 0) {
             ndivisor *= 2;
             factor++;
         }
+        // Set the dividend as the difference between the highest doubled divisor and the dividend
         dividend -= ndivisor;
         quotient += 2**factor;
     }
+    // Make the quotient negative if applicable and return
     if (!pos) {
         quotient *= -1;
     }
