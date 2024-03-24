911258c613479f2e42a7f163eda67bc67f339363
theleuth <121182711+louiecasula@users.noreply.github.com>
2024-03-24 11:27:20 -0400

    Time: 73 ms (80.55%), Space: 56.8 MB (95.04%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.js
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.js
@@ -3,12 +3,18 @@
  * @return {number}
  */
 var findDuplicate = function(nums) {
-    // Sort nums
-    nums = nums.sort();
-    // Iterate until a num repeats
-    for (let i = 0; i < nums.length - 1; i++) {
-        if (nums[i] == nums[i + 1]) {
-            return nums[i];
-        }
+    //// Linked List, Floyd's Cycle Method ////
+    // Iterate with slow and fast pointers until they intersect
+    let slow = 0, fast = 0;
+    do {
+        slow = nums[slow];
+        fast = nums[nums[fast]];
+    } while (slow !== fast);
+    // Iterate from the beginning and from the intersection, return the new intersection
+    let slow2 = 0;
+    while (slow !== slow2) {
+        slow = nums[slow];
+        slow2 = nums[slow2];
     }
+    return slow;
 };
