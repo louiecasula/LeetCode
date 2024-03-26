57db9540db87443cdece34bdefb73c5bbc202bb9
theleuth <121182711+louiecasula@users.noreply.github.com>
2024-03-26 18:21:23 -0400

    Time: 85 ms (37.88%), Space: 58.2 MB (59.90%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var firstMissingPositive = function(nums) {
+    //// The Constant-Space, Negative Marking Solution ////
+    // Replace all nums less than 1 and greater than n with n + 1
+    for (let i = 0; i < nums.length; i++) { // Try this with map
+        if (nums[i] < 1 || nums[i] > nums.length) {
+            nums[i] = nums.length + 1;
+        }
+    }
+    console.log(nums);
+    // Iterate again, mark the num at the index of the current num as negative
+    for (let i = 0; i < nums.length; i++) { 
+        let num = Math.abs(nums[i]);
+        if (num > nums.length) {
+            continue;
+        }
+        num--;
+        if (nums[num] > 0) {
+            nums[num] *= -1;
+        } 
+    }
+    console.log(nums);
+    // Iterate again again, if a num is positive, return it
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] >= 0) {
+            return i + 1;
+        }
+    }
+    // Return n + 1
+    return nums.length + 1
+};
