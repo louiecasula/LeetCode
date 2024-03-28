045262ffd25f13d8f0ae33bd1ff7ae8b3d3461a3
theleuth <121182711+louiecasula@users.noreply.github.com>
2024-03-28 10:39:58 -0400

    Time: 156 ms (60.18%), Space: 63.8 MB (94.69%) - LeetHub

--- 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.js
+++ 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.js
@@ -5,22 +5,20 @@
  */
 var maxSubarrayLength = function(nums, k) {
     //// Sliding Window Approach ////
-    // Keep a frequency map, max length var, counter, and left and right pointers
-    let freq = {}, max = 0, count = 0, l = 0, r = 0;
+    // Keep a frequency map, max length var, and left and right pointers
+    let freq = {}, max = 0, l = 0, r = 0;
     // Iterate until right pointer reaches the end
     while (r < nums.length) {
         // Update freq of each right val
         freq[nums[r]] = nums[r] in freq? freq[nums[r]] + 1: 1;
-        // If a val's freq exceeds k, decrement lefts val's freq and counter, shift pointer
+        // If a val's freq exceeds k, decrement lefts val's freq, shift pointer
         while (freq[nums[r]] > k) {
             freq[nums[l]]--;
             l++;
-            count--;
         }
-        // Shift right, increment counter, and update max length
+        // Shift right and update max length
+        max = Math.max(max, r - l + 1);
         r++;
-        count++;
-        max = Math.max(max, count);
     }
     // Return max length
     return max;
