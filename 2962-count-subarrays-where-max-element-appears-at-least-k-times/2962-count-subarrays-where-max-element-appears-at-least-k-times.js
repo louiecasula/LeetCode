a768e99fe2a24ede2b20f8e6230738a9c9da33ff
theleuth <121182711+louiecasula@users.noreply.github.com>
2024-03-29 12:08:26 -0400

    Time: 115 ms (25.00%), Space: 62.3 MB (57.14%) - LeetHub

--- 2962-count-subarrays-where-max-element-appears-at-least-k-times/2962-count-subarrays-where-max-element-appears-at-least-k-times.js
+++ 2962-count-subarrays-where-max-element-appears-at-least-k-times/2962-count-subarrays-where-max-element-appears-at-least-k-times.js
@@ -6,19 +6,18 @@
 var countSubarrays = function(nums, k) {
     //// Sliding Window Approach ////
     // Keep an output variable, max value, max count, left and right pointers
-    let out = 0, max = Math.max(...nums), max_count = 0, l = 0, r = 0;
+    let out = 0, max = Math.max(...nums), max_count = 0, l = 0;
     // Iterate until right reaches the end of array
-    while (r < nums.length) {
+    for (let r = 0; r < nums.length; r++) {
         if (nums[r] == max) { max_count++; }
         // If max_count == k, shift left until it doesn't
         while (max_count == k) {
             if (nums[l] == max || nums[l] == nums[r]) { max_count--; }
             l++;
         }
-        // Else, add window size to output
+        // Add window size to output
         out += r - l + 1;
-        r++;
     }
-    // Return total - output
+    // Return total subarrays - output
     return ((nums.length * (nums.length + 1)) / 2) - out;
 };
