31fa8f28c0b21a9c9541e19dbb64cd97091561f2
theleuth <121182711+louiecasula@users.noreply.github.com>
2024-04-02 11:20:04 -0400

    Time: 63 ms (63.51%), Space: 54.7 MB (24.53%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.js
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.js
@@ -4,20 +4,18 @@
  * @return {number}
  */
 var longestOnes = function(nums, k) {
-    // Keep an output, zCount, and left and right pointers
-    let out = 0, zCount = 0, l = 0;
+    // Keep left and right pointers
+    let l = 0, r = 0;
     // Slide da window
-    for (let r = 0; r < nums.length; r++) {
-        // If right element is 0, increment zCount
-        if (nums[r] == 0) { zCount++; }
-        // If zCount > k, shift left until it's a zero, decrement zCount
-        while (zCount > k) {
-            if (nums[l] == 0) { zCount--; }
+    for (; r < nums.length; r++) {
+        // If right element is 0, decrement k
+        if (nums[r] == 0) { k--; }
+        // If k is negative, shift left, increment k
+        if (k < 0) {
+            if (nums[l] == 0) { k++; }
             l++;
         }
-        // If window > output, update output
-        out = Math.max(out, r - l + 1);
     }
-    // Return output
-    return out;
+    // Return window size
+    return r - l;
 };
