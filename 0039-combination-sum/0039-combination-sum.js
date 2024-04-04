83a08fa799c10fc49a2c3a73e12eb5d725d07a44
theleuth <121182711+louiecasula@users.noreply.github.com>
2024-04-04 13:56:38 -0400

    Time: 81 ms (38.81%), Space: 53.8 MB (81.10%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.js
+++ 0039-combination-sum/0039-combination-sum.js
@@ -8,27 +8,22 @@ var combinationSum = function(candidates, target) {
     let out = [];
     candidates.sort((a, b) => a - b);
     // Make a backtrack function that tests all possible solutions
-    var backtrack = function(currArr, currSum) {
+    var backtrack = function(start, currArr, currSum) {
         // Base case: current array sums to target
         if (currSum === target) {
-            // Check if current array is already in output array
-            currArr.sort((a, b) => a - b);
-            for (let i = 0; i < out.length; i++) {
-                if (JSON.stringify(out[i]) === JSON.stringify(currArr)) {
-                    return;
-                }
-            }
-            out.push(currArr);
+            out.push([...currArr]);
             return;
         }
         // Iterate nums in input, recursively running function with each num added to current array
-        for (let i = 0; i < candidates.length; i++) {
+        for (let i = start; i < candidates.length; i++) {
             if (currSum + candidates[i] <= target) {
-                backtrack([...currArr, candidates[i]], currSum + candidates[i]);
+                currArr.push(candidates[i]);
+                backtrack(i, currArr, currSum + candidates[i]);
+                currArr.pop();
             } else { return; }
         }
     }
     // Run backtrack function and return output
-    backtrack([], 0);
+    backtrack(0, [], 0);
     return out;
 };
