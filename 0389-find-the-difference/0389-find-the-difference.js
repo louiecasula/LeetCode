d0dd4532aebfd55b8b918f7c2c64f2057017d4c8
theleuth <121182711+louiecasula@users.noreply.github.com>
2024-04-04 14:11:17 -0400

    Time: 71 ms (10.33%), Space: 51 MB (48.48%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.js
+++ 0389-find-the-difference/0389-find-the-difference.js
@@ -7,8 +7,6 @@ var findTheDifference = function(s, t) {
     // Sort both strings
     s = s.split("").sort();
     t = t.split("").sort();
-    console.log(s)
-    console.log(t)
     // Iterate, if t's element doesn't equal s', return it
     for (let i = 0; i < t.length; i++) {
         if (s[i] !== t[i]) { return t[i]; }
