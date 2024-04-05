83b236245090ce546de63327065e94781a7ef234
theleuth <121182711+louiecasula@users.noreply.github.com>
2024-04-05 07:48:39 -0400

    Time: 57 ms (68.69%), Space: 52.1 MB (15.09%) - LeetHub

---
+++ 1544-make-the-string-great/1544-make-the-string-great.js
@@ -0,0 +1,33 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var makeGood = function(s) {
+    let out = null;
+    // Function that recursively makes string "good"
+    var recursion = function(st) {
+        // Keep an output string and a flag that changes if string is altered
+        let currString = "";
+        let flag = true;
+        // Iterate, if current's ASCII !== next +- 32, append to output
+        for (let i = 0; i < st.length; i++) {
+            if (st.charCodeAt(i) === st.charCodeAt(i + 1) + 32 ||
+            st.charCodeAt(i) === st.charCodeAt(i + 1) - 32) {
+                flag = false;
+                i++;
+            }
+            else {
+                currString += st[i];
+            }
+        }
+        // If flag is false, call with resulting string, else return string
+        if (flag === true) {
+            out = currString;
+        } else {
+            recursion(currString);
+        }
+    }
+    // Return recursive function's output
+    recursion(s);
+    return out;
+};
