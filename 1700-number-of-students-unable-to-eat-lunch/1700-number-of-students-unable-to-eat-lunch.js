38ec8ef66b494c2c44fd87876872c1efd432a24d
theleuth <121182711+louiecasula@users.noreply.github.com>
2024-04-08 08:55:59 -0400

    Time: 61 ms (18.73%), Space: 48.6 MB (84.30%) - LeetHub

--- 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.js
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.js
@@ -8,14 +8,14 @@ var countStudents = function(students, sandwiches) {
     // Keep iterating while the counter < size of students OR zero
     while (counter < students.length && students.length > 0) {
         // If the first student === first sandwich, shift student and sandwich, set counter to zero
-        let first = students.shift();
-        if (first === sandwiches[0]) {
+        if (students[0] === sandwiches[0]) {
+            students.shift()
             sandwiches.shift();
             counter = 0;
         }
         // Else, shift and push student, increment counter
         else {
-            students.push(first);
+            students.push(students.shift());
             counter++;
         }
     }
