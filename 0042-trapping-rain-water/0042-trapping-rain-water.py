e1cf8b00bd098d3674ecb5bf62168500f92a4dc5
theleuth <121182711+louiecasula@users.noreply.github.com>
2024-04-12 10:34:00 -0400

    Time: 108 ms (39.25%), Space: 18.6 MB (39.10%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,18 +1,24 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        # If length of height is less than 3, return 0
-        if len(height) < 3:
-            return 0
-        # Keep an output counter and a left and right max
+        # Keep an output counter, left and right pointers, and left and right maxes
         out = 0
+        l, r = 0, len(height) - 1
+        lmax, rmax = height[l], height[r]
         # Iterate,
-        for i in range(len(height)):
-        # Find max height to left and right of element
-            left = max(height[:i]) if i > 0 else 0
-            right = max(height[i + 1:]) if i < len(height) - 1 else 0
-            # Increment output by min of max heights - current height
-            water = min(left, right) - height[i]
-            if water > 0:
-                out += water
+        while l < r:
+            # Sum the lesser of lmax and rmax - l or r
+            # If the sum is positive, add to output
+            # Update whichever max was target
+            # Move the pointer that had the lesser max
+            if rmax < lmax:
+                r -= 1
+                sum = rmax - height[r]
+                out += sum if sum > 0 else 0
+                rmax = max(rmax, height[r])
+            else:
+                l += 1
+                sum = lmax - height[l]
+                out += sum if sum > 0 else 0
+                lmax = max(lmax, height[l])
         # Return output
         return out
