a5a5fba613bc63a9cfe3cb9b341ea379d3aa6434
theleuth <121182711+louiecasula@users.noreply.github.com>
2024-05-03 12:37:11 -0400

    Time: 36 ms (48.34%), Space: 16.7 MB (13.68%) - LeetHub

--- 0165-compare-version-numbers/0165-compare-version-numbers.py
+++ 0165-compare-version-numbers/0165-compare-version-numbers.py
@@ -1,12 +1,12 @@
 class Solution:
     def compareVersion(self, version1: str, version2: str) -> int:
         # Split both versions by "."
-        v1 = deque(version1.split("."))
-        v2 = deque(version2.split("."))
+        version1 = deque(version1.split("."))
+        version2 = deque(version2.split("."))
         # Shift each num in v1 and v2,
-        while v1 or v2:
-            s1 = int(v1.popleft()) if len(v1) > 0 else 0
-            s2 = int(v2.popleft()) if len(v2) > 0 else 0
+        while version1 or version2:
+            s1 = int(version1.popleft()) if len(version1) > 0 else 0
+            s2 = int(version2.popleft()) if len(version2) > 0 else 0
             # If v1 > v2, return 1. If v2 > v1, return -1
             if s1 > s2:
                 return 1
