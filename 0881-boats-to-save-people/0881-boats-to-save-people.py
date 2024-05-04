f21e4244309e5720eff2fb0f836ff8e7d49522f4
theleuth <121182711+louiecasula@users.noreply.github.com>
2024-05-04 10:12:47 -0400

    Time: 355 ms (37.75%), Space: 23.5 MB (25.52%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.py
+++ 0881-boats-to-save-people/0881-boats-to-save-people.py
@@ -2,13 +2,14 @@ class Solution:
     def numRescueBoats(self, people: List[int], limit: int) -> int:
         # Keep a counter variable
         boats = 0
-        # Sort people
+        # Sort people and make it a queue
         people.sort()
+        people = deque(people)
         # Add the largest and smallest to reach limit, increment counter when limit reached
         while people:
             sublim = people.pop()
             if people and people[0] + sublim <= limit:
-                sublim += people.pop(0)
+                sublim += people.popleft()
             boats += 1
         # Return counter
         return boats
