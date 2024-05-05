f5b09418041f6d48c58f4316a2f34b33615385e2
theleuth <121182711+louiecasula@users.noreply.github.com>
2024-05-05 15:51:43 -0400

    Time: 92 ms (40.74%), Space: 20.7 MB (98.83%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,19 +1,10 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        # Keep an output array
-        out = []
         # Map frequency of all elements in nums
         freq = defaultdict(int)
         for num in nums:
             freq[num] += 1
-        # In the range of k, add max frequency num, then remove it from map
-        while k > 0:
-            top = (0, 0)
-            for x in freq.items():
-                if x[1] > top[1]:
-                    top = x
-            out.append(top[0])
-            del freq[top[0]]
-            k -= 1
-        # Return output
-        return out
+        # Sort frequency map by values in descending order
+        sort = {k: v for k, v in sorted(freq.items(), key=lambda item: item[1], reverse=True)}
+        # Return slice of frequency map keys up to k
+        return list(sort.keys())[:k]
