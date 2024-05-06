6ed0c1809ebb2ce9d7786a43672ca829792876fb
theleuth <121182711+louiecasula@users.noreply.github.com>
2024-05-06 12:53:57 -0400

    Time: 416 ms (61.52%), Space: 62.3 MB (13.17%) - LeetHub

--- 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.py
+++ 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.py
@@ -5,32 +5,14 @@
 #         self.next = next
 class Solution:
     def removeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        # Keep a list of node vals and two pointer nodes
-        vals = deque()
-        curr = prev = head
-        # Traverse ll, add all vals to list
-        while curr:
-            vals.append(curr.val)
-            curr = curr.next
-        curr = prev
-        # Iterate queue backwards, save each val as a tuple with the max val so far
-        currMax = float(-inf)
-        for i in range(len(vals) -1, -1, -1):
-            if vals[i] > currMax:
-                currMax = vals[i]
-            vals[i] = (vals[i], currMax)
-        # Traverse ll and queue simultaneously, keep a boolean if head has been established
-        isHead = False
-        while vals:
-            currVal = vals.popleft()
-            # If curr val isn't the max val from there on, remove it
-            if vals and currVal[0] != currVal[1]:
-                prev.next = curr.next
-            else:
-                if not isHead:
-                    head = curr
-                    isHead = True
-                prev = curr
-            curr = curr.next
-        # Return head
+        # Base case: end of ll is reached
+        if head is None or head.next is None:
+            return head
+        # Recursive case: continue traversing ll
+        remaining = self.removeNodes(head.next)
+        # If current val is less than remaining, skip (return remaining)
+        if head.val < remaining.val:
+            return remaining
+        # Else, link nodes
+        head.next = remaining
         return head
