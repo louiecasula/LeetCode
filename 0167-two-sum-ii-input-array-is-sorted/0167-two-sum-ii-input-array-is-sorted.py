375a8feef29bb7ee719ce368ecfb3f2e09e76172
theleuth <121182711+louiecasula@users.noreply.github.com>
2024-05-12 16:44:36 -0400

    Time: 107 ms (45.30%), Space: 17.3 MB (98.45%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -4,12 +4,14 @@ class Solution:
         l, r = 0, len(numbers) - 1
         # While left is less than right,
         while l < r:
-            # If sum of left and right nums is less than target, increment left
-            if numbers[l] + numbers[r] < target:
+            # Save the sum of left and right nums
+            s = numbers[l] + numbers[r]
+            # If sum is less than target, increment left
+            if s < target:
                 l += 1
-            # If sum of left and right nums is more than target, decrement right
-            elif numbers[l] + numbers[r] > target:
+            # If sum is more than target, decrement right
+            elif s > target:
                 r -= 1
-            # If sum of left and right nums is equal to target, return indices
+            # If sum is equal to target, return indices
             else:
                 return [l + 1, r + 1]
