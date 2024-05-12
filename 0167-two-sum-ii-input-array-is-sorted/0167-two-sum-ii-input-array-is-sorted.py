e4b2fc325f1658eb6248c7f648613579bfecf8c2
theleuth <121182711+louiecasula@users.noreply.github.com>
2024-05-12 16:42:37 -0400

    Time: 111 ms (24.42%), Space: 17.6 MB (25.62%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -1,10 +1,15 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        # Iterate numbers
-        n = len(numbers)
-        while numbers:
-            # If list contains negative (current num - target), return the indexes of both
-            curr = numbers.pop()
-            needed = -(curr - target)
-            if needed in numbers:
-                return(numbers.index(needed) + 1, n - (n - len(numbers)) + 1)
+        # Keep a left and right pointer starting at the beginning and end of numbers
+        l, r = 0, len(numbers) - 1
+        # While left is less than right,
+        while l < r:
+            # If sum of left and right nums is less than target, increment left
+            if numbers[l] + numbers[r] < target:
+                l += 1
+            # If sum of left and right nums is more than target, decrement right
+            elif numbers[l] + numbers[r] > target:
+                r -= 1
+            # If sum of left and right nums is equal to target, return indices
+            else:
+                return [l + 1, r + 1]
