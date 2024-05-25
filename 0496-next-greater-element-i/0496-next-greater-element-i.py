df90d9566d7982930451afa2d4afa304269af2ba
theleuth <121182711+louiecasula@users.noreply.github.com>
2024-05-25 15:19:37 -0400

    Time: 47 ms (67.56%), Space: 16.8 MB (44.93%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,10 +1,18 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        for i in range(len(nums1)):
-            for j in range(nums2.index(nums1[i]), len(nums2)):
-                if nums2[j] > nums1[i]:
-                    nums1[i] = nums2[j]
-                    break
-            else:
-                nums1[i] = -1
-        return nums1
+        # Keep a stack and an output array
+        out = [-1] * len(nums1)
+        stack = []
+        # Map each num in nums1 to its index
+        map = {n: i for i, n in enumerate(nums1)}
+        # Iterate nums2,
+        for num in nums2:
+            # If current num is greater than top stack val,
+            while stack and num > stack[-1]:
+                # Set output at index of popped val to current num
+                out[map[stack.pop()]] = num
+            # If current num is in nums1, add to stack
+            if num in nums1:
+                stack.append(num)
+        # Return output
+        return out
