bcf032e0f6215912b5a52260998d62d775e54154
theleuth <121182711+louiecasula@users.noreply.github.com>
2024-05-28 10:58:31 -0400

    Time: 65 ms (54.81%), Space: 17.9 MB (21.29%) - LeetHub

--- 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.py
+++ 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.py
@@ -4,22 +4,21 @@ class Solution:
         N = len(s)
         diff = [0] * N
         # Keep a variable to count diffs greater than maxCost
-        high = 0
-        for i in range(N):
-            diff[i] = abs(ord(s[i]) - ord(t[i]))
-            if diff[i] > maxCost:
-                high += 1
-        # If all diffs are greater than maxCost, return 0
-        if high == N:
-            return 0
+        high = 0            
         # Return the length of the longest difference subarray that sums to maxCost
         out = 0
         l = 0
         cost = 0
         for r in range(N):
+            diff[r] = abs(ord(s[r]) - ord(t[r]))
+            if diff[r] > maxCost:
+                high += 1
             cost += diff[r]
             while cost > maxCost and l < r:
                 cost -= diff[l]
                 l += 1
             out = max(out, r - l + 1)
+        # If all diffs are greater than maxCost, return 0
+        if high == N:
+            return 0
         return out
