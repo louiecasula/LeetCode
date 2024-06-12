55974501d3844d3641b1f2c8911c3725b06795ac
theleuth <121182711+louiecasula@users.noreply.github.com>
2024-06-12 11:02:12 -0400

    Time: 40 ms (36.00%), Space: 16.6 MB (33.92%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,15 +3,18 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # Keep pointer, l
-        l = 0
-        # From 0 -> 2,
-        for color in range(3):
-            # Iterate nums,
-            for i in range(l, len(nums)):
-                # If num == i, swap with l pointer and increment l
-                if nums[i] == color:
-                    temp = nums[i]
-                    nums[i] = nums[l]
-                    nums[l] = temp
+        # Keep pointers for start, end, and iteration
+        l = i = 0
+        r = len(nums) - 1
+        # While iterating pointer <= end pointer,
+        while i <= r:
+            # Swap current num if it's a 0 or 2 with start or end pointer
+            if nums[i] == 2:
+                nums[r], nums[i] = nums[i], nums[r]
+                r -= 1
+            else:
+                if nums[i] == 0:
+                    nums[l], nums[i] = nums[i], nums[l]
                     l += 1
+                # Increment iterating pointer
+                i += 1
