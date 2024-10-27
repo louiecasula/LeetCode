e82bf047a94a603498a9cfede652a59cebdfecdd
theleuth <121182711+louiecasula@users.noreply.github.com>
2024-10-27 10:23:36 -0400

    Time: 203 ms (11.59%), Space: 37.4 MB (5.14%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.py
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.py
@@ -2,7 +2,7 @@ class Solution:
     def countSquares(self, matrix: List[List[int]]) -> int:
         # Keep a cache and an output variable
         ROWS, COLS = len(matrix), len(matrix[0])
-        cache = [[-1 for x in range(COLS)] for y in range(ROWS)]
+        cache = {}
         out = 0
         # DFS function to find maximal square from given coordinate
         def dfs(r, c):
@@ -10,15 +10,15 @@ class Solution:
             if r == ROWS or c == COLS or matrix[r][c] == 0:
                 return 0
             # If coordinate already in cache, return val
-            if cache[r][c] >= 0:
-                return cache[r][c]
+            if (r, c) in cache:
+                return cache[(r, c)]
             # Recursive case: run down, down-right, and right and cache the minimum val for current coordinate + 1
-            cache[r][c] = 1 + min(
+            cache[(r, c)] = 1 + min(
                 dfs(r + 1, c),
                 dfs(r + 1, c + 1),
                 dfs(r, c + 1)
             )
-            return cache[r][c]
+            return cache[(r, c)]
         # Iterate rows,
         for r in range(ROWS):
             # Iterate columns,
