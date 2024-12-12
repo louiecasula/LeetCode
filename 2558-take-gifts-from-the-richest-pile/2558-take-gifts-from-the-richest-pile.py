34180c5df512796afb24179c2f2b0f47d8b915a1
theleuth <121182711+louiecasula@users.noreply.github.com>
2024-12-12 13:30:20 -0500

    Time: 4 ms (62.99%), Space: 17.4 MB (20.60%) - LeetHub

--- 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.py
+++ 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.py
@@ -1,10 +1,14 @@
 class Solution:
     def pickGifts(self, gifts: List[int], k: int) -> int:
+        # Make gifts a heap
+        gifts = [-x for x in gifts]
+        heapify(gifts)
         # Iterate to k,
-        for i in range(k):
-            # Sort gifts
-            gifts = sorted(gifts)
-            # Change largest gift to floor square root
-            gifts[-1] = floor(sqrt(gifts[-1]))
+        while k > 0:
+            # Pop from heap
+            largest = -heappop(gifts)
+            # Push floor sqrt of popped element
+            heappush(gifts, -isqrt(largest))
+            k -= 1
         # Return sum of gifts
-        return sum(gifts)
+        return -sum(gifts)
