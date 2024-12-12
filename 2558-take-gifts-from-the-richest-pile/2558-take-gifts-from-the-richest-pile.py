9daea953c484c5fa0204689d811748b8e9363da6
theleuth <121182711+louiecasula@users.noreply.github.com>
2024-12-12 13:20:41 -0500

    Time: 31 ms (29.00%), Space: 17.5 MB (13.92%) - LeetHub

--- 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.py
+++ 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.py
@@ -2,9 +2,9 @@ class Solution:
     def pickGifts(self, gifts: List[int], k: int) -> int:
         # Iterate to k,
         for i in range(k):
-            # Find largest gift
-            largest = max(gifts)
+            # Sort gifts
+            gifts = sorted(gifts)
             # Change largest gift to floor square root
-            gifts[gifts.index(largest)] = floor(sqrt(largest))
+            gifts[-1] = floor(sqrt(gifts[-1]))
         # Return sum of gifts
         return sum(gifts)
